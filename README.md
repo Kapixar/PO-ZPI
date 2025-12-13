62cc404156f85be8dbbae5f8a90530a66f115352
Kapixar <85899949+Kapixar@users.noreply.github.com>
2025-12-13 16:56:53 +0100

    initialized frontend
    - react
    - tailwind
    - beercss
    - typescript

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # PO-ZPI
+
 Backend and frontend app for designing software course.
+
+# Frontend
+
+Run the project:`
+`cd frontend`
+`npm i`- for updating dependencies (and first time usage)`npm run dev`
+
+### Dependencies docs
+
+- BeerCSS
+  - [Github](https://github.com/beercss/beercss)
+  - [Demo and docs](https://www.beercss.com/)
+  - [Docs on github](https://github.com/beercss/beercss/blob/main/docs/INDEX.md)
+- Tailwind
+  - https://tailwindcss.com/docs/styling-with-utility-classes
---
+++ frontend/app/routes/test.tsx
@@ -0,0 +1,37 @@
+import type { Route } from "./+types/test";
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "Test" }];
+}
+
+export default function Test() {
+  return (
+    <>
+
+
+      <nav className="left max">
+        <a>
+          <i>home</i>
+          <div>Pulpit</div>
+        </a>
+        <a>
+          <i>search</i>
+          <div>Tematy</div>
+        </a>
+        <a>
+          <i>more_vert</i>
+          <div>Mój profil</div>
+        </a>
+      </nav>
+      <main className="responsive">
+        <nav className="group split">
+            <button className="left-round">Button</button>
+            <button className="right-round">
+                <i>keyboard_arrow_down</i>
+            </button>
+            </nav>
+
+      </main>
+    </>
+  );
+}
